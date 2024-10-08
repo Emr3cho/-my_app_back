7b705a860e6d39fe8cd11e0b85aa3523c296b739
Emr3cho <emre.metin2002@gmail.com>
2024-10-08 13:24:58 +0300

    add initial code and basic configurations

---
+++ src/test/java/eshop/myecommerceshop/MyEcommerceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package eshop.myecommerceshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEcommerceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
